9797ec6ff4b1df58f7edc4ac77b2dce02e8f2117
Masud Rana <44302742+masudpro@users.noreply.github.com>
2020-08-27 19:58:18 +0600

    Add files via upload

---
+++ build/precache-manifest.c74948e246932490ef2d2ce3b243cb97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25fd99fc76179a2d64f48610d07a540f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adcce308cffb0398ae94",
+    "url": "/static/css/main.414d2312.chunk.css"
+  },
+  {
+    "revision": "740410a8f9e2d746f227",
+    "url": "/static/js/2.e8cd9c8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e8cd9c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adcce308cffb0398ae94",
+    "url": "/static/js/main.dc6df12a.chunk.js"
+  },
+  {
+    "revision": "3925b0889d7cc5e82afc",
+    "url": "/static/js/runtime-main.6f0102fe.js"
+  }
+]);
